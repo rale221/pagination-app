d245db73b1f2626c4c92d5ad73bf53da5837fb8a
Rastko <rastkodjurica@yahoo.com>
2021-06-30 23:31:04 -0400

    pagination button

--- main.js
+++ main.js
@@ -19,31 +19,49 @@ const list_items = [
      "Item 18",
      "Item 19",
      "Item 20"
-]
+];
 
-const list_element = document.getElementById('list')
-const pagination_element = document.getElementById('pagination')
+const list_element = document.getElementById('list');
+const pagination_element = document.getElementById('pagination');
 
-let curent_page = 1
+let current_page = 1
 let rows = 5
 
 function DisplayList(items, wrapper, rows_per_page, page){
-     wrapper.innerHTML = ''
-     page --
-     let start = rows_per_page * page
-     let end = start + rows_per_page
-     let paginatedItems = items.slice(start, end)
+     wrapper.innerHTML = '';
+     page--;
+     let start = rows_per_page * page;
+     let end = start + rows_per_page;
+     let paginatedItems = items.slice(start, end);
      
-     for (let i= 0; i<paginatedItems.length; i++){
-          let item = paginatedItems[i]
-          let item_element = document.createElement('div')
-          item_element.classList.add('item')
-          item_element.innerText = item
+     for (let i= 0; i < paginatedItems.length; i++){
+          let item = paginatedItems[i];
+          let item_element = document.createElement('div');
+          item_element.classList.add('item');
+          item_element.innerText = item;
 
-          wrapper.appendChild(item_element)
+          wrapper.appendChild(item_element);
 
      }
 }
 
+function SetupPagination(items, wrapper, rows_per_page){
+     wrapper.innerHTML = '';
 
-DisplatList(list_items, list_element, rows, current_page)
+     let page_count = Math.ceil(items.length / rows_per_page);
+     for (let i =1; i <page_count + 1; i++){
+         let btn =  PaginationButton(i);
+         wrapper.appendChild(btn);
+     }
+}
+
+function PaginationButton(page){
+     let button = document.createElement('button');
+     button.innerText = page;
+
+     if (current_page == page) button.classList.add('active');
+
+     return button;
+}
+DisplayList(list_items, list_element, rows, current_page)
+SetupPagination(list_items, pagination_element, rows)
